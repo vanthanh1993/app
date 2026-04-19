08ae4ad9bd99e21fe947073fc52f8e8372e25633
vanthanh1993 <levanha90@gmail.com>
2026-04-20 03:47:48 +0700

    Delete models directory

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user import User
-from .product import Product
-from .imei import IMEI
-from .supplier import Supplier
-from .purchase import Purchase, PurchaseItem
-from .sale import Sale, SaleItem
-from .payment import SupplierPayment
-from .customer import Customer 
-from .customer_payment import CustomerPayment
